1e9a55c637df342e13e1bad31b4f2aec2bd6c0ef
Hung Liu <hungliu983@gmail.com>
2025-04-21 23:13:53 -0500

    updates for db

---
+++ migrations/versions/add_cougar_id_to_user_add_cougar_id_field_to_user_model.py
@@ -0,0 +1,24 @@
+"""Add cougar_id field to User model
+
+Revision ID: add_cougar_id_to_user
+Revises: 8d7c3b307536
+Create Date: 2025-04-21 03:02:44.239951
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'add_cougar_id_to_user'
+down_revision = '8d7c3b307536'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
